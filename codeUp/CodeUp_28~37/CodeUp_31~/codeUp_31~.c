827339ff2e024f6b659d0188342c36fb5a9a6a38
jongdu <parksn5029@naver.com>
2020-03-01 19:29:12 +0900

    "Study_ 28~37"

---
+++ codeUp/CodeUp_28~37/CodeUp_31~/codeUp_31~.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void main() {
+	/*unsigned int num1;
+	scanf("%u",&num1);
+	printf("%u", num1);	codeUp_28*/
+
+	/*double num5;
+	scanf("%lf",&num5);
+	printf("%lf", num5);	codeUp_29*/
+
+	/*long long int num1;
+	scanf("%lld", &num1);
+	printf("%lld", num1);	codeUp_30*/
+
+	/*int num1;
+	scanf("%d",&num1);
+	printf("%o",num1);	codeUp_31*/
+
+	/*int num1;
+	scanf("%d", &num1);
+	printf("%x", num1);	codeUp_32*/
+
+	/*int num1;
+	scanf("%d", &num1);
+	printf("%X", num1);	codeUp_33*/
+
+	/*int num1;
+	scanf("%o", &num1);
+	printf("%d",num1);	codeUp_34*/
+
+	/*int num1;
+	scanf("%x", &num1);
+	printf("%o",num1);	codeUp_35*/
+
+	/*char ch1;
+	scanf("%c", &ch1);
+	printf("%d",ch1);	codeUp_36*/
+
+	/*int ch1;
+	scanf("%d", &ch1);
+	printf("%c",ch1);	codeUp_37*/
+
+
+}
