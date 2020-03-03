c989a023097a7696e7d035a622e05fcd8f3c9f66
jong du <parksn5029@naver.com>
2020-03-03 13:55:39 +0900

    "Study_ 38~48"

---
+++ codeUp/codeUp_38~/codeUp_38~/codeUp_38~.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void main() {
+	/*int num1, num2, sum;
+	scanf("%d %d",&num1,&num2);
+
+	sum = num1 + num2;
+	printf("두 수의 합계는 :%d\n",sum);		codeUp_38 */
+
+	/*int num1, num2;
+	long long int sum;
+	scanf("%d %d", &num1, &num2);
+
+	sum = num1 + num2;
+	printf("두 수의 합계는 :%lld\n", sum);		codeUp_39 */
+
+	/*int num1;
+	scanf("%d",&num1);
+	printf("%d",-num1);		codeUp_40 */
+
+	/*char ch1;
+	scanf("%c",&ch1);
+	printf("%c",ch1+1);		codeUp_41 */
+
+	/*int b;
+	float a;
+	scanf("%f %d",&a,&b);
+	printf("%f",a/b);			codeUp_42 */
+
+	/*int b, a;
+	scanf("%d %d", &a, &b);
+	printf("%d", a % b);		codeUp_43 */
+
+	/*int a = 11 ,b = 11;
+	printf("%d %d\n",++a,b++);
+	printf("%d %d",a,b);		codeUp_44 */
+
+	/*int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+	printf("%.2f\n", (float)a / b);		codeUp_45 */
+
+	/*int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int score = a + b + c;
+	int	avg = score/3;
+	printf("%d %d",score,avg);		codeUp_46 */
+
+	/*int a;
+	scanf("%d", &a);
+	printf("%d",a<<1);		codeUp_47 */
+	
+	int a, b;
+	scanf("%d %d",&a,&b);
+	printf("%d",a<<b);
+}
