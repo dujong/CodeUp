7946d02cb1ba0f5f3b35b4f81cbc65357d0b32a3
jongdu <parksn5029@naver.com>
2020-03-04 20:42:53 +0900

    "Study_ codeUp_49~60"

---
+++ codeUp/CodeUp_49~/CodeUp_49~/codeUp_49~.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+typedef unsigned char bool;
+#define true 1
+#define false 0
+void main() {
+
+	/*int num1, num2;
+	scanf("%d %d",&num1,&num2);
+	printf("%d",num1>num2);		codeUp_49 */
+
+	/*int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%d", num1 == num2);		codeUp_50 */
+
+	/*int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%d", num1 <= num2);		codeUp_51*/
+
+	/*int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	printf("%d", num1 != num2);		codeUp_52*/
+
+	/*bool test = true;
+	printf("%d",!test);		codeUp_53*/
+
+	/*bool test1, test2;
+	scanf("%d %d",&test1,&test2);
+	if (test1 == 1 && test2 == 1) {
+		printf("%d %d",test1,test2);
+	}								codeUp_54*/
+
+
+	/*bool test1, test2;
+	scanf("%d %d", &test1, &test2);
+	if (test1 == 1 || test2 == 1) {
+		printf("%d %d", test1, test2);
+	}								codeUp_55*/
+
+	/*bool test1, test2;
+	scanf("%d %d", &test1, &test2);
+	if ((test1&&!test2) || (!test1&&test2)) {
+		printf("%d %d", test1, test2);
+	}								codeUp_56*/
+
+	/*bool test1, test2;
+	scanf("%d %d", &test1, &test2);
+	if ((test1==test2) || (test1==test2)) {
+		printf("%d %d", test1, test2);
+	}								codeUp_57*/
+
+	/*bool test1, test2;
+	scanf("%d %d", &test1, &test2);
+	if (test1==0 && test2 ==0) {
+		printf("%d %d", test1, test2);
+	}								codeUp_58*/
+
+	/*int a = 1;
+	printf("%d",~a);		codeUp_59*/
+
+	/*int a, b;
+	scanf("%d %d",&a,&b);
+	printf("%d",a&b);		coduUp+60*/
+}
